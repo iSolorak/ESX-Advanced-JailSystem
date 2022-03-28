a7b6fe77388a3141f1925248421586b58f4a0f45
Fezz <43589668+FBFezz@users.noreply.github.com>
2022-03-28 10:13:05 +0100

    Update client.lua
    Small optimisations and formatting, need to look at this again

--- jailsystem/client.lua
+++ jailsystem/client.lua
@@ -1,90 +1,60 @@
-Config  = {}
-
 ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
---
 
-local injail = false
-local checked = false 
+local injail, checked = false, false 
 
-
-Citizen.CreateThread(function() --Thread to countdown
-  
+CreateThread(function() --Thread to countdown
     while true do
-    Citizen.Wait(0)
-      if injail == true then 
-        Citizen.Wait(60000)
-       TriggerServerEvent('timeremoval')
-      end
+    Wait(0)
+        if injail == true then 
+            Wait(60000)
+            TriggerServerEvent('timeremoval')
         end
-    
-end)
-
-
-
-Citizen.CreateThread(function() --Thread for tp back
-  
-  while true do
-  Citizen.Wait(0)
-  --print(injail)
-    while injail do 
-      Citizen.Wait(0)
-      
-      if GetDistanceBetweenCoords(vector3(1723.37,2525.76,45.56),GetEntityCoords(GetPlayerPed(-1))) > 100 then 
-        SetEntityCoords(GetPlayerPed(-1),1723.37,2525.76,45.56,true,false,false,false)
-      end
-     
     end
-
-  end
 end)
 
-Citizen.CreateThread(function() -- Check if player injail or not
-  
-  while true do
-    Citizen.Wait(0)
-    if checked == false then 
-     
-      TriggerServerEvent('test')
-      checked = true 
+CreateThread(function() --Thread for tp back
+    while true do
+        Wait(0)
+        while injail do 
+            Wait(0)
+            if GetDistanceBetweenCoords(vector3(1723.37,2525.76,45.56),GetEntityCoords(PlayerPedId())) > 100 then 
+                SetEntityCoords(PlayerPedId(),1723.37,2525.76,45.56,true,false,false,false)
+            end     
+        end
     end
-
-  end
 end)
 
+CreateThread(function() -- Check if player injail or not
+    while true do
+        Wait(0)
+        if checked == false then 
+            TriggerServerEvent('test')
+            checked = true 
+        end
+    end
+end)
 
-
-
-RegisterNetEvent('tpprison')  -- TP's to prin when command executed
+RegisterNetEvent('tpprison')  -- TP's to prison when command executed
 AddEventHandler('tpprison',function(id,time) --tp to the jail and starts timer 
     a = tonumber(id)
     b = tonumber(time)
-   if GetPlayerServerId(PlayerId()) == a then 
-    SetEntityCoords(GetPlayerPed(-1),1723.37,2525.76,45.56,true,false,false,false)
-    
-    injail = true 
-    
+    if GetPlayerServerId(PlayerId()) == a then 
+        SetEntityCoords(GetPlayerPed(-1),1723.37,2525.76,45.56,true,false,false,false)
+        injail = true 
     end 
-
 end)
-
 
 RegisterNetEvent('outofjail')
 AddEventHandler('outofjail',function()
-  Citizen.Wait(2000)
-  SetEntityCoords(GetPlayerPed(-1),1857.53,2601.89,45.67,true,false,false,false)
-  
-
+    Wait(2000)
+    SetEntityCoords(GetPlayerPed(-1),1857.53,2601.89,45.67,true,false,false,false)
 end)
-
 
 RegisterNetEvent('injail') -- From server if he is injail or not
 AddEventHandler('injail',function(bool)
-injail = bool
-
-
-
+    injail = bool
 end)
 
 function DrawHudText(text,colour,coordsx,coordsy,scalex,scaley) --courtesy of driftcounter
